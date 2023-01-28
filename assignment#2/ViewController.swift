d50655f33b3da6ba31277bc95aeb1680b11ce55c
Cory Ward <randallcoryward@gmail.com>
2023-01-28 13:50:15 -0600

    Initial Commit

---
+++ assignment#2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment#2
+//
+//  Created by user230914 on 1/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
